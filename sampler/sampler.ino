8f3a61d5f86187c8c0f9e93e0e73346bd8ed72bd
db2inst1 <db2inst1@webafrica.org.za>
2015-04-14 08:28:56 +0200

    Reduce startup delay. Add rest between analog samples.

--- sampler/sampler.ino
+++ sampler/sampler.ino
@@ -1,5 +1,6 @@
-const int SAMPLE_DELAY = 250;
+const int SAMPLE_INTERVAL_MS = 250;
 const int LED_PIN = 13;
+const int LED_DELAY = 50;
 
 const int ACHANS = 6;
 const int SAMPLE_MAX = 1023;
@@ -8,8 +9,30 @@ const float SAMPLE_HIGH = 90.0;
 const char FIELD_NUM_SPLIT = ':';
 const char FIELD_SEP = ',';
 
+void blink_led() {
+  digitalWrite(LED_PIN, HIGH);
+  delay(LED_DELAY);
+  digitalWrite(LED_PIN, LOW);
+}
+
+bool led_on = false;
+void toggle_led() {
+  if (!led_on) {
+    digitalWrite(LED_PIN, HIGH);
+    led_on = true;
+  } else {
+    digitalWrite(LED_PIN, LOW);
+    led_on = false;
+  }
+}
+
 void setup() {
   pinMode(LED_PIN, OUTPUT);
+  // warm-up
+  for (int i=0; i<20; i++) {
+    toggle_led();
+    delay(LED_DELAY);
+  }
   Serial.begin(9600);
 }
 
@@ -19,6 +42,9 @@ int normalized = 0;
 void loop() {
   for (int c=0; c<ACHANS; c++) {
     sampled[c] = analogRead(c);
+    // Pause here for 10 x 100us between pins
+    // http://arduino.cc/en/Tutorial/AnalogInputPins
+    delay(1);
   }
   for (int c=0; c<ACHANS; c++) {
     // write the field type
@@ -45,6 +71,6 @@ void loop() {
   }
   high_value = false;
   // rest here for a moment
-  delay(SAMPLE_DELAY);
+  delay(SAMPLE_INTERVAL_MS);
 }
 
