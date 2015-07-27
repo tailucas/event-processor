cea512573eef933837b5f9f9f934b0203203708c
db2inst1 <db2inst1@webafrica.org.za>
2015-07-27 21:16:44 +0200

    More info.

--- hello.py
+++ hello.py
@@ -5,3 +5,9 @@ print "hello python!"
 import subprocess
 df = subprocess.Popen(["df", "-h"], stdout=subprocess.PIPE)
 print df.communicate()[0]
+
+r = subprocess.Popen(["cat", "/etc/*-release"], stdout=subprocess.PIPE)
+print r.communicate()[0]
+
+import time
+time.sleep(3600)
