c2709e91b9c50d5de3cdbedbd45ae9de7ed804fb
db2inst1 <db2inst1@webafrica.org.za>
2015-09-06 13:58:11 +0200

    add logger

--- hello.py
+++ hello.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python
 import os
+import sys
+import logging
+import logging.handlers
 
-print "hello python!"
+APP = os.path.basename(__file__)
+log = logging.getLogger(APP)
+
+log.setLevel(logging.DEBUG)
+syslog_handler = logging.handlers.SysLogHandler(address='/dev/log')
+formatter = logging.Formatter('%(name)s [%(levelname)s] %(message)s')
+syslog_handler.setFormatter(formatter)
+log.addHandler(syslog_handler)
+stream_handler = logging.StreamHandler(stream=sys.stdout)
+log.addHandler(stream_handler)
+
+log.info("Hello App!")
 
 import subprocess
 df = subprocess.Popen(["df", "-h"], stdout=subprocess.PIPE)
