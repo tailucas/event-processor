750bd05508634ec3b2a7d70805da561bf33f21b2
db2inst1 <db2inst1@webafrica.org.za>
2015-09-10 20:09:21 +0200

    signal handler stay alive log only remove shell debug

--- hello.py
+++ hello.py
@@ -1,42 +1,32 @@
 #!/usr/bin/python
 import os
+import signal
 import sys
 import logging
 import logging.handlers
-from time import sleep
-
-sleep(5)
+from time import sleep, time
 
 APP = os.path.basename(__file__)
 log = logging.getLogger(APP)
 
-log.setLevel(logging.DEBUG)
-syslog_handler = logging.handlers.SysLogHandler(address='/dev/log')
-formatter = logging.Formatter('%(name)s [%(levelname)s] %(message)s')
-syslog_handler.setFormatter(formatter)
-log.addHandler(syslog_handler)
-stream_handler = logging.StreamHandler(stream=sys.stdout)
-log.addHandler(stream_handler)
+def handler(signum, frame):
+    log.info('Goodbye signal {}'.format(signum))
+    exit(0)
 
-log.info("Hello App!")
+if __name__ == "__main__":
+    signal.signal(signal.SIGTERM, handler)
 
-for key,value in os.environ.items():
-    print "{}: {}".format(key, value)
+    log.setLevel(logging.DEBUG)
+    syslog_handler = logging.handlers.SysLogHandler(address='/dev/log')
+    formatter = logging.Formatter('%(name)s [%(levelname)s] %(message)s')
+    syslog_handler.setFormatter(formatter)
+    log.addHandler(syslog_handler)
+    stream_handler = logging.StreamHandler(stream=sys.stdout)
+    log.addHandler(stream_handler)
 
-import subprocess
-#df = subprocess.Popen(["df", "-h"], stdout=subprocess.PIPE)
-#print df.communicate()[0]
+    for key,value in os.environ.items():
+        log.info("{}: {}".format(key, value))
 
-r = subprocess.Popen(["cat", "/etc/os-release"], stdout=subprocess.PIPE)
-print r.communicate()[0]
-
-for key, value in os.environ.items():
-    print '{}={}'.format(key, value)
-
-r = subprocess.Popen(["cat", "/etc/rsyslog.conf"], stdout=subprocess.PIPE)
-print r.communicate()[0]
-
-r = subprocess.Popen(["tail", "/var/log/syslog"], stdout=subprocess.PIPE)
-print r.communicate()[0]
-
-sleep(3600)
+    while True:
+        log.info('hello {}'.format(time()))
+        sleep(1)
--- start_hello.sh
+++ start_hello.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 set -eux
 
-echo "rsyslog server is ${RSYSLOG_SERVER}"
 if [ -n "$RSYSLOG_SERVER" ]; then
   gosu echo "*.*          ${RSYSLOG_SERVER}" >> /etc/rsyslog.conf
   gosu service rsyslog restart
