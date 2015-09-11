b0363ed42d5a05cc9274d90303ff3880e298d2e5
db2inst1 <db2inst1@webafrica.org.za>
2015-09-11 22:24:17 +0200

    inspect rsyslog.conf

--- start_hello.sh
+++ start_hello.sh
@@ -3,6 +3,7 @@ set -eux
 
 if [ -n "$RSYSLOG_SERVER" ]; then
   echo "*.*          ${RSYSLOG_SERVER}" >> /etc/rsyslog.conf
+  tail /etc/rsyslog.conf
   service rsyslog restart
 fi
 python /app/hello.py
