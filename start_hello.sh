ed48b13ae4f1dff65e21bd317d75eb3041bbae82
db2inst1 <db2inst1@webafrica.org.za>
2015-09-12 07:30:37 +0200

    Unset variable checks.

--- start_hello.sh
+++ start_hello.sh
@@ -3,7 +3,7 @@ set -eux
 
 # remove unnecessary kernel drivers
 KMOD=$(lsmod | grep w1_gpio)
-if [ -n "$KMOD" ]; then
+if [ -n "${KMOD:-}" ]; then
   # unload this to prevent these kernel messages,
   # apparently when GPIO is not connected.
   # w1_master_driver w1_bus_master1: Family 0 for 00.ef9b00000000.48 is not registered.
@@ -11,7 +11,7 @@ if [ -n "$KMOD" ]; then
 fi
 lsmod
 
-if [ -n "$RSYSLOG_SERVER" ]; then
+if [ -n "${RSYSLOG_SERVER:-}" ]; then
   echo "*.*          @${RSYSLOG_SERVER}" >> /etc/rsyslog.conf
   tail /etc/rsyslog.conf
   service rsyslog restart
