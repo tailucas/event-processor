3fcf69b35fda2dc9620280263ae6c0f34ab5b724
db2inst1 <db2inst1@webafrica.org.za>
2015-09-12 07:49:08 +0200

    Remove kmod check.

--- start_hello.sh
+++ start_hello.sh
@@ -2,13 +2,7 @@
 set -eux
 
 # remove unnecessary kernel drivers
-KMOD=$(lsmod | grep w1_gpio)
-if [ -n "${KMOD:-}" ]; then
-  # unload this to prevent these kernel messages,
-  # apparently when GPIO is not connected.
-  # w1_master_driver w1_bus_master1: Family 0 for 00.ef9b00000000.48 is not registered.
-  rmmod w1_gpio
-fi
+rmmod w1_gpio
 lsmod
 
 if [ -n "${RSYSLOG_SERVER:-}" ]; then
@@ -16,6 +10,5 @@ if [ -n "${RSYSLOG_SERVER:-}" ]; then
   tail /etc/rsyslog.conf
   service rsyslog restart
 fi
-
 
 sudo -u app python /app/hello.py
