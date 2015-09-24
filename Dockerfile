06adfeb51b9f3d8696618db19af99f4b75e249c7
db2inst1 <db2inst1@webafrica.org.za>
2015-09-24 14:57:34 +0200

    Move activities that depend on environment variable to ENTRYPOINT.

--- Dockerfile
+++ Dockerfile
@@ -29,24 +29,11 @@ RUN pip list --outdated
 
 # FTP
 EXPOSE 21
-RUN groupadd -r "${FTP_USER}" && useradd -r -g "${FTP_USER}" "${FTP_USER}"
-RUN mkdir -p "/home/${FTP_USER}/"
-RUN mkdir -p "${APP_SNAPSHOTS_ROOT_DIR}"
-RUN ln -s "${APP_SNAPSHOTS_ROOT_DIR}/" "/home/${FTP_USER}/ftp"
-RUN chown -R "/home/${FTP_USER}/"
-RUN chown -R "${APP_SNAPSHOTS_ROOT_DIR}/"
-RUN chmod a-w "/home/${FTP_USER}/ftp"
-RUN echo "${FTP_USER}:${FTP_PASSWORD}" | chpasswd
 
 # zmq
 EXPOSE 5556
 
 COPY . /app
 COPY ./start_hello.sh /
-
-# non-root users
-RUN groupadd -r app && useradd -r -g app app
-RUN chown -R app /app/
-RUN chown app /start_hello.sh
 
 ENTRYPOINT ["/start_hello.sh"]
--- start_hello.sh
+++ start_hello.sh
@@ -10,10 +10,24 @@ if [ -n "${RSYSLOG_SERVER:-}" ]; then
   service rsyslog restart
 fi
 
+groupadd -r "${FTP_USER}" && useradd -r -g "${FTP_USER}" "${FTP_USER}"
+mkdir -p "/home/${FTP_USER}/"
+mkdir -p "${APP_SNAPSHOTS_ROOT_DIR}"
+ln -s "${APP_SNAPSHOTS_ROOT_DIR}/" "/home/${FTP_USER}/ftp"
+chown -R "/home/${FTP_USER}/"
+chown -R "${APP_SNAPSHOTS_ROOT_DIR}/"
+chmod a-w "/home/${FTP_USER}/ftp"
+echo "${FTP_USER}:${FTP_PASSWORD}" | chpasswd
+
 cat /etc/vsftpd.conf | python /app/config_interpol /app/config/vsftpd.conf | sort | tee /etc/vsftpd.conf.new
 mv /etc/vsftpd.conf /etc/vsftpd.conf.backup
 mv /etc/vsftpd.conf.new /etc/vsftpd.conf
 tail /etc/vsftpd.conf
 service vsftpd restart
+
+# non-root users
+RUN groupadd -r app && useradd -r -g app app
+RUN chown -R app /app/
+RUN chown app /start_hello.sh
 
 su -p app -c 'python /app/hello.py'
