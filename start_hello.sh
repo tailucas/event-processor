1c5bcf40887f86cccd534c84bd25e6fdf6b4c35f
db2inst1 <db2inst1@webafrica.org.za>
2015-09-24 21:26:42 +0200

    Use app user from environment variable. Share app group with app and FTP user.

--- start_hello.sh
+++ start_hello.sh
@@ -10,14 +10,18 @@ if [ -n "${RSYSLOG_SERVER:-}" ]; then
   service rsyslog restart
 fi
 
-groupadd -r "${FTP_USER}" && useradd -r -g "${FTP_USER}" "${FTP_USER}"
+# groups
+groupadd -r "${APP_GROUP}"
+
+useradd -r -g "${FTP_USER}" "${APP_GROUP}"
 mkdir -p "/home/${FTP_USER}/"
 mkdir -p "${APP_SNAPSHOTS_ROOT_DIR}"
 ln -s "${APP_SNAPSHOTS_ROOT_DIR}" "/home/${FTP_USER}/ftp"
-chown -R "${FTP_USER}" "/home/${FTP_USER}/"
-chown -R "${FTP_USER}" "${APP_SNAPSHOTS_ROOT_DIR}"
-chmod a-w "/home/${FTP_USER}/ftp"
 mkdir -p "/home/${FTP_USER}/ftp/files"
+chown -R "${FTP_USER}:${APP_GROUP}" "/home/${FTP_USER}/"
+chown -R "${FTP_USER}:${APP_GROUP}" "${APP_SNAPSHOTS_ROOT_DIR}"
+chmod a-w "/home/${FTP_USER}/ftp"
+
 echo "${FTP_USER}:${FTP_PASSWORD}" | chpasswd
 
 cat /etc/vsftpd.conf | python /app/config_interpol /app/config/vsftpd.conf | sort | tee /etc/vsftpd.conf.new
@@ -27,8 +31,8 @@ tail /etc/vsftpd.conf
 service vsftpd restart
 
 # non-root users
-groupadd -r app && useradd -r -g app app
-chown -R app /app/
-chown app /start_hello.sh
+useradd -r -g "${APP_USER}" "${APP_GROUP}"
+chown -R "${APP_USER}:${APP_GROUP}" /app/
+chown "${APP_USER}:${APP_GROUP}" /start_hello.sh
 
-su -p app -c 'python /app/hello.py'
+su -p "${APP_USER}" -c 'python /app/hello.py'
