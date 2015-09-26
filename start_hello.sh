eccc61b36d82eb3965afc3213f059b27dcc22c4b
db2inst1 <db2inst1@webafrica.org.za>
2015-09-26 08:03:28 +0200

    start rsyslog before supervisor

--- start_hello.sh
+++ start_hello.sh
@@ -5,6 +5,7 @@ set -eux
 if [ -n "${RSYSLOG_SERVER:-}" ]; then
   echo "*.*          @${RSYSLOG_SERVER}" >> /etc/rsyslog.conf
   tail /etc/rsyslog.conf
+  service rsyslog restart
 fi
 
 # remove unnecessary kernel drivers
