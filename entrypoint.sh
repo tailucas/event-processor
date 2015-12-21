1137f2d43f1aace840557749f14d90f9ae5711d0
db2inst1 <db2inst1@webafrica.org.za>
2015-12-21 17:54:04 +0200

    Refresh entrypoint: 1) Cache results from external calls. 2) Common layout for all apps.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,36 @@
 #!/bin/bash
 set -eu
+
 # Resin API key
 export RESIN_API_KEY="${RESIN_API_KEY:-$API_KEY_RESIN}"
+# root user access, prefer key
+if [ -n "$SSH_AUTHORIZED_KEY" ]; then
+  echo "$SSH_AUTHORIZED_KEY" > /root/.ssh/authorized_keys
+  chmod 600 /root/.ssh/authorized_keys
+elif [ -n "$ROOT_PASSWORD" ]; then
+  echo "root:${ROOT_PASSWORD}" | chpasswd
+  sed -i 's/PermitRootLogin without-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+  # SSH login fix. Otherwise user is kicked off after login
+  sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd
+fi
+
+
 set -x
+
 
 # Run user
 export APP_USER="${APP_USER:-app}"
 export APP_GROUP="${APP_GROUP:-app}"
+
+# groups
+groupadd -f -r "${APP_GROUP}"
+
+# non-root users
+id -u "${APP_USER}" || useradd -r -g "${APP_GROUP}" "${APP_USER}"
+chown -R "${APP_USER}:${APP_GROUP}" /app/
+# non-volatile storage
+chown -R "${APP_USER}:${APP_GROUP}" /data/
+
 
 TZ_CACHE=/data/localtime
 # a valid symlink
@@ -18,51 +42,45 @@ else
   cp -a /etc/localtime "$TZ_CACHE"
 fi
 
+
 # remote system logging
-if [ -n "${RSYSLOG_HOSTNAME:-}" ] && ! grep -q "$RSYSLOG_HOSTNAME" /etc/hostname; then
-  echo "${RSYSLOG_HOSTNAME}" > /etc/hostname
-  # apply the new hostname
-  /etc/init.d/hostname.sh start
-  # update hosts
-  echo "127.0.1.1 ${RSYSLOG_HOSTNAME}" >> /etc/hosts
+HN_CACHE=/data/hostname
+if [ -e "$HN_CACHE" ]; then
+  export DEVICE_NAME="$(cat "$HN_CACHE")"
+else
+  export DEVICE_NAME="$(python /app/resin --get-device-name)"
+  echo "$DEVICE_NAME" > "$HN_CACHE"
 fi
+echo "$DEVICE_NAME" > /etc/hostname
+# apply the new hostname
+/etc/init.d/hostname.sh start
+# update hosts
+echo "127.0.1.1 ${DEVICE_NAME}" >> /etc/hosts
+unset DEVICE_NAME
+
 if [ -n "${RSYSLOG_SERVER:-}" ] && ! grep -q "$RSYSLOG_SERVER" /etc/rsyslog.conf; then
   echo "*.*          @${RSYSLOG_SERVER}" | tee -a /etc/rsyslog.conf
 fi
 
-# root user access, try a cert
-if [ -n "$SSH_AUTHORIZED_KEY" ]; then
-  echo "$SSH_AUTHORIZED_KEY" > /root/.ssh/authorized_keys
-  chmod 600 /root/.ssh/authorized_keys
+
+# configuration update
+export ETH0_IP="$(/sbin/ifconfig eth0 | grep 'inet addr' | awk '{ print $2 }' | cut -f2 -d ':')"
+SUB_CACHE=/data/sub_src
+if [ -e "$SUB_CACHE" ]; then
+  export SUB_SRC="$(cat "$SUB_CACHE")"
 else
-  echo 'root:resin' | chpasswd
-  sed -i 's/PermitRootLogin without-password/PermitRootLogin yes/' /etc/ssh/sshd_config
-  # SSH login fix. Otherwise user is kicked off after login
-  sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd
+  export SUB_SRC="$(python /app/resin --get-devices | grep -v "$ETH0_IP" | paste -d, -s)"
+  echo "$SUB_SRC" > "$SUB_CACHE"
 fi
+# application configuration (no tee for secrets)
+cat /app/config/app.conf | python /app/config_interpol > "/app/${APP_NAME}.conf"
+unset ETH0_IP
+unset SUB_SRC
+
 
 # remove unnecessary kernel drivers
 rmmod w1_gpio||true
 
-# groups
-groupadd -f -r "${APP_GROUP}"
-
-# configuration update
-if [ ! -e /app/event_processor.conf ] || grep '%\(.*\)s$' /app/event_processor.conf; then
-  export ETH0_IP="$(/sbin/ifconfig eth0 | grep 'inet addr' | awk '{ print $2 }' | cut -f2 -d ':')"
-  export SUB_SRC="$(python /app/resin --get-devices | grep -v "$ETH0_IP" | paste -d, -s)"
-  # application configuration (no tee for secrets)
-  cat /app/config/event_processor.conf | python /app/config_interpol > /app/event_processor.conf
-  unset ETH0_IP
-  unset SUB_SRC
-fi
-
-# non-root users
-id -u "${APP_USER}" || useradd -r -g "${APP_GROUP}" "${APP_USER}"
-chown -R "${APP_USER}:${APP_GROUP}" /app/
-# non-volatile storage
-chown "${APP_USER}:${APP_GROUP}" /data/
-chown "${APP_USER}:${APP_GROUP}" /data/*
 # so app user can make the noise
 adduser "${APP_USER}" audio
 
