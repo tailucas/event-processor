2e2c0d348fe5f4db90a6a9288c8efb8ca3e1ed5d
db2inst1 <db2inst1@webafrica.org.za>
2016-03-04 20:37:11 +0200

    download awslogs setup

--- awslogs-agent-setup.sh
+++ awslogs-agent-setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -eu
 
+wget https://s3.amazonaws.com/aws-cloudwatch/downloads/latest/awslogs-agent-setup.py -O /app/awslogs-agent-setup.py
 python /app/awslogs-agent-setup.py -n -r "eu-west-1" -c /app/config/awslogs-config
 # remove the service and nanny (supervisor does this)
 update-rc.d awslogs remove
