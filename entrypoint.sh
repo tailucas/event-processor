5db666f8780bae0f252ea4a9627caa757b14e0df
db2inst1 <db2inst1@webafrica.org.za>
2016-07-14 20:31:00 +0200

    fix config file extension

--- entrypoint.sh
+++ entrypoint.sh
@@ -18,7 +18,7 @@ fi
 if [ -n "${NGROK_AUTH_TOKEN:-}" ]; then
   ./app/ngrok authtoken  --config /app/ngrok.yml "${NGROK_AUTH_TOKEN}"
 fi
-cat /app/config/ngrok_frontend.yml | python /app/config_interpol > /app/ngrok_frontend.conf
+cat /app/config/ngrok_frontend.yml | python /app/config_interpol > /app/ngrok_frontend.yml
 
 set -x
 
