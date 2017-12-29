f051bb551b8c48ac121a06f93df8c17393ca82a8
Tai Lucas <tglucas@gmail.com>
2017-12-29 07:44:05 +0000

    Quote URL for runtime syntax error.

--- ngrok_setup.sh
+++ ngrok_setup.sh
@@ -2,7 +2,7 @@
 set -eux
 set -o pipefail
 
-NGROK_URL=$(curl -s https://ngrok.com/download | hxnormalize -x 2>/dev/null | hxselect '#dl-linux-arm' | egrep -o 'https.*\.zip')
+NGROK_URL=$(curl -s 'https://ngrok.com/download' | hxnormalize -x 2>/dev/null | hxselect '#dl-linux-arm' | egrep -o 'https.*\.zip')
 TMP_DIR=$(mktemp -d -t ngrok-XXX)
 wget -nv -P "${TMP_DIR}" "${NGROK_URL}"
 unzip "${TMP_DIR}/*.zip" -d /app/
