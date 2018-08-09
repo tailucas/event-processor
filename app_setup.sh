c9170d4063c9f13f57a7dc228edf2a0977512368
Tai Lucas <tglucas@gmail.com>
2018-08-09 19:37:25 +0200

    Simplify app setup script.

--- app_setup.sh
+++ app_setup.sh
@@ -2,14 +2,9 @@
 set -eux
 set -o pipefail
 
-# User application
-pushd /opt/
-if [ ! -d app ]; then
-  pyvenv app
-fi
-. app/bin/activate
+pyvenv /opt/app/
+. /opt/app/bin/activate
 # work around wheel stupidity
 pip3 install wheel
 pip3 install -r "/opt/app/config/requirements.txt"
 deactivate
-popd
