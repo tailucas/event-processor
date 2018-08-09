cb9bd32a5a553feac61f8b1a565a3bc3d9be45a0
Tai Lucas <tglucas@gmail.com>
2018-08-09 19:40:07 +0200

    venv workaround.

--- app_setup.sh
+++ app_setup.sh
@@ -3,7 +3,10 @@ set -eux
 set -o pipefail
 
 pyvenv /opt/app/
+# work around venv stupidity
+set +u
 . /opt/app/bin/activate
+set -u
 # work around wheel stupidity
 pip3 install wheel
 pip3 install -r "/opt/app/config/requirements.txt"
