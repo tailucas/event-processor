b427f1316ab5a8315198ff3ca18021822a37f88d
Tai Lucas <tglucas@gmail.com>
2018-08-09 19:44:18 +0200

    Simplify app setup script to work around venv stupidity.

--- app_setup.sh
+++ app_setup.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
-set -eux
+set -e
 set -o pipefail
 
 pyvenv /opt/app/
-# work around venv stupidity
-set +u
 . /opt/app/bin/activate
-set -u
 # work around wheel stupidity
 pip3 install wheel
 pip3 install -r "/opt/app/config/requirements.txt"
