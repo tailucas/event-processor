793cf563dda5654ddacec0deddcf870cea88f916
Tai Lucas <tglucas@gmail.com>
2018-12-09 13:52:41 +0200

    Filter for ARM.

--- ngrok_setup.sh
+++ ngrok_setup.sh
@@ -2,7 +2,7 @@
 set -eux
 set -o pipefail
 
-NGROK_URL=$(curl -s 'https://ngrok.com/download' | hxnormalize -x 2>/dev/null | hxselect '#dl-linux-arm' 2>/dev/null | egrep -o 'https.*\.zip' | tail -1)
+NGROK_URL=$(curl -s 'https://ngrok.com/download' | hxnormalize -x 2>/dev/null | hxselect '#dl-linux-arm' 2>/dev/null | egrep -o 'https.*\arm.zip' | tail -1)
 TMP_DIR=$(mktemp -d -t ngrok-XXX)
 wget -nv -P "${TMP_DIR}" "${NGROK_URL}"
 unzip "${TMP_DIR}/*.zip" -d /opt/app/
