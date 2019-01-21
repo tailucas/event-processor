ad1eb692a971850ed47714caa7222c75e940b957
Tai Lucas <tglucas@gmail.com>
2019-01-21 09:40:35 +0200

    ssh bug workaround

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,6 +15,8 @@ elif [ -n "$ROOT_PASSWORD" ]; then
   # SSH login fix. Otherwise user is kicked off after login
   sed 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' -i /etc/pam.d/sshd
 fi
+# https://bugs.launchpad.net/ubuntu/+source/openssh/+bug/45234
+mkdir -p /run/sshd
 # reload sshd
 service ssh reload
 
