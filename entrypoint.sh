ef8abd483ae1e926690d8a30e38fcf5f14686aec
Tai Lucas <tglucas@gmail.com>
2019-01-21 13:29:13 +0200

    Debug systemd.

--- entrypoint.sh
+++ entrypoint.sh
@@ -150,4 +150,5 @@ done
 for systemdsvc in app ngrok; do
   systemctl start "${systemdsvc}"&
 done
+sleep infinity &
 exec env DBUS_SYSTEM_BUS_ADDRESS=unix:path=/run/dbus/system_bus_socket /sbin/init quiet systemd.show_status=0
