ee1d617100da54a399837eaaf8985d7a936913df
Tai Lucas <tglucas@gmail.com>
2020-05-21 20:58:13 +0200

    Override pip timeout.

--- app_setup.sh
+++ app_setup.sh
@@ -4,6 +4,8 @@ set -o pipefail
 
 python -m venv --system-site-packages /opt/app/
 . /opt/app/bin/activate
+# work around timeouts to www.piwheels.org
+export PIP_DEFAULT_TIMEOUT=60
 # work around wheel stupidity
 pip3 install wheel
 pip3 install -r "/opt/app/config/requirements.txt"
