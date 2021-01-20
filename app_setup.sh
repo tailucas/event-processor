7713da22e5ad9949f1053a11d68ad76596f93275
Tai Lucas <tglucas@gmail.com>
2021-01-20 08:11:21 +0200

    Rehome requirements.txt. App setup enhancements.

--- app_setup.sh
+++ app_setup.sh
@@ -2,11 +2,29 @@
 set -e
 set -o pipefail
 
+# system updates
+
+# work around pip stupidity
+python -m pip install --upgrade pip
+# work around setuptools stupidity
+python -m pip install --upgrade setuptools
+# work around wheel stupidity
+python -m pip install --upgrade wheel
+
+# virtual-env updates
+
 python -m venv --system-site-packages /opt/app/
 . /opt/app/bin/activate
 # work around timeouts to www.piwheels.org
 export PIP_DEFAULT_TIMEOUT=60
+
+# work around pip stupidity
+python -m pip install --upgrade pip
+# work around setuptools stupidity
+python -m pip install --upgrade setuptools
 # work around wheel stupidity
-pip3 install wheel
-pip3 install -r "/opt/app/config/requirements.txt"
+python -m pip install --upgrade wheel
+
+python -m pip install -r "/opt/app/requirements.txt"
+
 deactivate
