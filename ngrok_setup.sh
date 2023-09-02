c51edb158552e7eea834e801edc42959f89bb783
Tai Lucas <tglucas@gmail.com>
2023-09-02 13:35:37 +0200

    Fix ngrok download URL.

--- ngrok_setup.sh
+++ ngrok_setup.sh
@@ -2,7 +2,7 @@
 set -eux
 set -o pipefail
 
-NGROK_URL=$(curl -s 'https://ngrok.com/download' | hxnormalize -x 2>/dev/null | grep '#linux-dl-link' | grep -Eo 'https://.*linux-amd64.tgz' | tail -1)
+NGROK_URL=$(curl -s 'https://ngrok.com/download' | hxnormalize -x 2>/dev/null | grep -Eo 'https://.*linux-amd64.tgz' | tail -1)
 TMP_DIR=$(mktemp -d -t ngrok-XXXXXX)
 wget -nv -P "${TMP_DIR}" "${NGROK_URL}"
 tar -xvzf ${TMP_DIR}/*.tgz --directory /opt/app/
