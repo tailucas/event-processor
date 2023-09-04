2d5431e164f0a25b20a06e2c111fd21aa5462c2c
Tai Lucas <tglucas@gmail.com>
2023-09-04 15:30:08 +0200

    Update pylib.

--- connect_to_app.sh
+++ connect_to_app.sh
@@ -3,8 +3,8 @@ set -eu
 
 CONTAINER_ID=$(docker ps -qf name="$1")
 if [ -n "${CONTAINER_ID:-}" ]; then
-  docker exec -it "$CONTAINER_ID" /bin/sh
+  docker exec -it "$CONTAINER_ID" /bin/bash
 else
   echo "No existing container, starting new session."
-  docker-compose run app sh
+  docker-compose run app bash
 fi
