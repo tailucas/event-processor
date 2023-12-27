e803f4a0e152114814fa16ce36de53b5376ed812
Tai Lucas <tglucas@gmail.com>
2023-12-27 19:57:41 +0200

    Flesh out event handler.

--- src/main/java/tailucas/app/EventHandler.java
+++ src/main/java/tailucas/app/EventHandler.java
@@ -1,14 +1,21 @@
 package tailucas.app;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+//TODO: research RequestMapping
+//@RequestMapping("/app")
 public class EventHandler {
+
+	@Autowired
+	private Environment env;
 
 	@GetMapping("/")
 	public String index() {
-		return "meh";
+		return Thread.currentThread() + " spring.threads.virtual.enabled " + env.getProperty("spring.threads.virtual.enabled");
 	}
 
 }
